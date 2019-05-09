e6d401d60f0ac038d05744a2e1a062a72097af37
michelle570 <49167773+michelle570@users.noreply.github.com>
2019-05-09 09:17:20 -0400

    Before adding modal for wins and losses

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
+//counts wins and losses
 let loses = 0;
 let wins = 0;
 
+//variable to enable or disable movement when modals are open.
+let enable = false;
+
+//saving html spots
 const winSpan = document.querySelector('#wins');
 const lossSpan = document.querySelector('#loses');
 
@@ -15,9 +20,9 @@ var Enemy = function(x, y, speed, sprite) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
     this.x < 500 ? this.x += this.speed * dt : this.x = -100;
-    //check collitions
+
+    //check for collitions
     checkCollisions(this.x, this.y);
 };
 
@@ -26,7 +31,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+//Player Function
 var Player = function(x, y) {
     this.x = x;
     this.y = y;
@@ -40,13 +45,10 @@ Player.prototype.update = function() {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-// This class requires an update(), render()* and a handleInput() method.
 
 Player.prototype.handleInput = function(key) {
     //console.log(key);
-  //  console.log(this.x + " " + this.y );
-    //switch statement for each key and how much to move.
-    //this is the enemy movement this.x < 500 ? this.x += this.speed * dt : this.x = -100;
+    //console.log(this.x + " " + this.y );
     if (key == "left" && this.x > 0) {
       this.x -= 100;
     }
@@ -59,11 +61,10 @@ Player.prototype.handleInput = function(key) {
     if (key == "down" &&  this.y < 400) {
       this.y += 85;
     }
-    //console.log(this.x + " " + this.y );
-    //check if at water & is safe
+
+    //check if player is at water & is safe
     if (this.y < 0) {
       console.log("at water");
-    //  alert("Congratulations!");
       wins += 1;
       resetPositions();
       console.log(wins);
@@ -74,31 +75,22 @@ Player.prototype.handleInput = function(key) {
 
 function checkCollisions(x,y) {
   //verify x & y against each enemy's x & y.
-
   if (player.x >= (x-50) && player.x <= (x+50) && player.y >= (y-20) && player.y <= (y+20)) {
-    //alert("Oh NO!!!!");
     loses -= 1;
     lossSpan.textContent = loses * -1;
     resetPositions();
   }
 };
 
+//Reset Player's positions
 function resetPositions() {
   player.x = 200;
   player.y = 400;
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-//starting positions
+// Now instantiate Enemies and players with starting positions
 let allEnemies = [new Enemy(100, 220, 120, "images/enemy-bug-purple.png"), new Enemy(200, 140, 150, "images/enemy-bug-blue.png"), new Enemy(0, 55, 250, "images/enemy-bug-green.png")];
 let player = new Player(200,400);
-
-
-//const scoreSpan = document.querySelector('#deck');
-//const winSpan = document.querySelector('#wins');
-//const lossSpan = document.querySelector('#loses');
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -109,6 +101,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
